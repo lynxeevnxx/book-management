88db532420538fb9b50baddd4f481ffc982df347
Tegar Deyustian <tegardm@gmail.com>
2021-07-07 16:49:00 +0700

    menyelesaikan fitur pencarian buku.

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,10 @@ const authorField = formSubmit.querySelector('#author');
 const isCompleteField = formSubmit.querySelector('#isComplete');
 const finishedContainer = document.querySelector('.finishedContent table');
 const notFinishedContainer = document.querySelector('.notFinishedContent table');
-
+const inputFilterField = document.getElementById('filterInput');
+const filterTable = document.getElementById('filterResultTable');
+const findBtn = document.getElementById('findBtn');
+const booksFounded = document.getElementById('booksFound')
 
 let bookInfo = {
     id : null,
@@ -18,10 +21,13 @@ let bookInfo = {
 
 formSubmit.addEventListener('submit', (e) => {
     e.preventDefault()
-   addBooks()
+    addBooks()
+    renderingListBooks()
     
 })
 
+
+findBtn.addEventListener('click',  filterBooks)
 
 const addBooks = () => {
     let timestamp = new Date().getUTCMilliseconds();
@@ -45,6 +51,50 @@ const addBooks = () => {
         localStorage.setItem('listBooks', stringfy);
 
     }
+}
+
+function filterBooks () {
+        const books = JSON.parse(localStorage.getItem('listBooks'));
+
+        filterTable.innerText = ''
+     
+       let filteredBooks =  books.filter(book => {
+            console.log(book)
+            return book.title.includes(inputFilterField.value) == true
+        })
+
+        booksFounded.innerText = `Buku Yang Ditemukan : ${filteredBooks.length}`
+
+        for (book of filteredBooks) {
+        const createTr = document.createElement('tr');
+        const titleEl = document.createElement('td');
+        const yearEl = document.createElement('td');
+        const authorEl = document.createElement('td');
+        const actionBtnElOne = document.createElement('td');
+        const actionBtnElTwo = document.createElement('td');
+        const actionBtnElThree = document.createElement('td');
+        const btnEdit = document.createElement('button');
+        const btnDelete = document.createElement('button');
+        const btnMove = document.createElement('button');
+        btnEdit.innerText ='Edit';
+        btnDelete.innerText = 'Delete';
+        btnMove.innerText = 'Move Book';
+        actionBtnElOne.appendChild(btnEdit);
+        actionBtnElTwo.appendChild(btnDelete);
+        actionBtnElThree.appendChild(btnMove);  
+        titleEl.innerText = book.title;
+        yearEl.innerText = book.yearReleased;
+        authorEl.innerText = book.author;
+        createTr.appendChild(titleEl);
+        createTr.appendChild(yearEl);
+        createTr.appendChild(authorEl);
+        createTr.appendChild(actionBtnElOne);
+        createTr.appendChild(actionBtnElTwo);
+        createTr.appendChild(actionBtnElThree);
+        filterTable.appendChild(createTr)
+        }
+
+        console.log(filteredBooks);
 }
 
 const renderingListBooks = () => {
@@ -76,7 +126,6 @@ const renderingListBooks = () => {
         createTr.appendChild(actionBtnElTwo);
         createTr.appendChild(actionBtnElThree);
 
-        console.log(createTr)
         if (book.isComplete) {
             finishedContainer.appendChild(createTr)
         } else if (!book.isComplete) {
@@ -86,3 +135,6 @@ const renderingListBooks = () => {
 }
 
 renderingListBooks()
+
+
+
