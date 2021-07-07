471ede693c66c253bfc33ecb8ae02d59945aefd1
Tegar Deyustian <tegardm@gmail.com>
2021-07-07 12:21:11 +0700

    menyelesaikan table

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,9 @@ const titleField = formSubmit.querySelector('#title');
 const yearReleasedField = formSubmit.querySelector('#year');
 const authorField = formSubmit.querySelector('#author');
 const isCompleteField = formSubmit.querySelector('#isComplete');
+const finishedContainer = document.querySelector('.finishedContent table');
+const notFinishedContainer = document.querySelector('.notFinishedContent table');
+
 
 let bookInfo = {
     id : null,
@@ -43,3 +46,34 @@ const addBooks = () => {
 
     }
 }
+
+const renderingListBooks = () => {
+    const dataBooks = JSON.parse(localStorage.getItem('listBooks'));
+    for (book of dataBooks) {
+        const createTr = document.createElement('tr');
+        const titleEl = document.createElement('td');
+        const yearEl = document.createElement('td');
+        const authorEl = document.createElement('td');
+        const actionBtnElOne = document.createElement('td');
+        const actionBtnElTwo = document.createElement('td');
+        const btnEdit = document.createElement('button');
+        const btnDelete = document.createElement('button');
+        btnEdit.innerText ='Edit';
+        btnDelete.innerText = 'Delete';
+        actionBtnElOne.appendChild(btnEdit);
+        actionBtnElTwo.appendChild(btnDelete);
+        titleEl.innerText = book.title;
+        yearEl.innerText = book.yearReleased;
+        authorEl.innerText = book.author;
+        createTr.appendChild(titleEl);
+        createTr.appendChild(yearEl);
+        createTr.appendChild(authorEl);
+        createTr.appendChild(actionBtnElOne);
+        createTr.appendChild(actionBtnElTwo);
+
+        console.log(createTr)
+        finishedContainer.appendChild(createTr)
+    }
+}
+
+renderingListBooks()
